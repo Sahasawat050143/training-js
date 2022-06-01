0741e345b2ea05385140a98a88fe218f9e0095ac
Sahasawat050143 <101787909+Sahasawat050143@users.noreply.github.com>
2022-06-01 17:47:35 +0700

    Learning spread Operator

--- variables_and_values/index.js
+++ variables_and_values/index.js
@@ -14,17 +14,42 @@
 
 
 //ข้อ 2
-const functionalLanguages = {
-    'purpose': 'computations',
-    'examples': ['Scala','F#','Haskell','Erlang']
-}
-let {purpose ,examples:[firstLang]} = functionalLanguages;
-console.log(firstLang);
+// const functionalLanguages = {
+//     'purpose': 'computations',
+//     'examples': ['Scala','F#','Haskell','Erlang']
+// }
+// let {purpose ,examples:[firstLang]} = functionalLanguages;
+// console.log(firstLang);
 
 //ข้อ 3
 // const person = ['Somchai',['Haha',{age:23}]];
 // let [firstname,[lastname,{age}]] = person;
 // console.log(firstname ,lastname ,age);
+
+// const person = {
+//     name : "Fear",
+//     age : 20
+// }
+// console.log(person?.name2)
+
+// const result = Math.min(9,2,16,1,3);
+// console.log(result);
+
+// const arr = [9,2,16,1,3];
+
+// console.log(...arr);
+
+// const seedcamp = {name:'เขียน Web ติดจรวด ด้วย React',date:'26-27พฤษภาคม'};
+// let seed1 = {location:'TB'};
+// let arr1 = {...seedcamp, ...seed1};
+// console.log(seedcamp);
+// console.log(arr1);
+
+
+const students = ['John','Jane','Joe','Josh'];
+const newStudent = [...students,'James'];
+console.log(newStudent);
+console.log(students);
 
 
 
